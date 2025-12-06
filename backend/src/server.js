2026ffc6710bff1ae36456164d1430908c7adbc5
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2025-12-06 14:16:59 +0600

    second

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//testing
+//testing aaa
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error('Database connection error:', err);
