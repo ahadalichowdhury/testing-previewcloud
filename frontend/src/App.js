53d35ec6e9bede2f5b5500f141c303dc00a3262e
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2025-12-07 13:27:31 +0600

    eight

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello ahad</h1>
+        <h1>Hello ahad ali</h1>
       </header>
     </div>
   );
