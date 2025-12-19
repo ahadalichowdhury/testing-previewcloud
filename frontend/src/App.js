cf4f5c6868adca156aaf421741574c9928e95244
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2025-12-19 11:41:18 +0600

    added get all data for manual create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 
-//testing dddddfgfgdfggdfd
+//testing
 function App() {
   const [backendMessage, setBackendMessage] = useState('')
   const [loading, setLoading] = useState(true)
